6d5efdc2e78d8dc8916cd04a1aba7398fe46ed36
crin706 <rosh-ka@mail.ru>
2024-04-25 18:39:51 +0300

    even number check

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        greeting();
+        evenNumber(inputNumber());
+    }
+
+    private static void greeting() {
+        System.out.println("Введите число:");
+    }
+
+    private static int inputNumber() {
+        int a = new Scanner(System.in).nextInt();
+        return a;
+    }
+    private static void evenNumber(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Ваше число четное.");
+        } else {
+            System.out.println("Ваше число нечетное.");
+        }
     }
 }
